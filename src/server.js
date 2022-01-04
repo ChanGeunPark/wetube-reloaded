fa6a4282fd4d597ae1e293627c6bc46894f0aca1
ChanGeunPark <91952938+ChanGeunPark@users.noreply.github.com>
2022-01-04 20:41:48 +0900

    commit test

--- src/server.js
+++ src/server.js
@@ -9,3 +9,5 @@ const handleListening = () => console.log(`server listenting on port http://loca
 app.listen(PORT, handleListening); //서버가 사람들이 뭔가를 요청할 때까지 기다리게 해준다. // 4000은 포트번호 handleListening은 콜백함수
 
 //3.1
+
+//commit test
