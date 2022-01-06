5f4a76b8c49997cbebcad2a46774cdc82335a70b
ChanGeunPark <design795@naver.com>
2022-01-06 22:03:49 +0900

    use route

---
+++ README.md
@@ -0,0 +1,18 @@
+!라우터를 만들기위한 플랜
+
+# Wetube Reloaded
+
+/ => Home
+/join -> Join
+/login -> Login
+/serch -> Search
+--> 글로벌 라우터
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+/video/watch -> Watch Video
+/video/edit -> Edit Video
+/video/delete -> Delete Video
+/video/comment -> comment on a video
+/video/comments/delete -> Delete A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -1,10 +1,44 @@
-import express from 'express';
+import express, { Router } from 'express';
 import morgan from 'morgan';//미들웨어 사용할때 유용함
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");//morgan라이브러리는 get,path, status code이 모든정보를 가지고 있다.
+app.use(logger);
+
+const globalRouter = express.Router();//글로벌 라우터
+const handleHome = (req,res) => res.send("Home");
+globalRouter.get("/", handleHome);//app.get대신 router.get을 사용한다.
+
+
+const usersRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Users");
+usersRouter.get("/edit", handleEditUser);
+
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+
+
+app.use("/", globalRouter);
+app.use("/users", usersRouter);
+app.use("/videos", videoRouter);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //express에선, route handler에는 event가 없지만 두개의 좋은 오브젝트가 있다.
 //하나는 req(request)오브젝트 다른하나는 res(response)오브젝트
@@ -20,24 +54,28 @@ const logger = morgan("dev");//morgan라이브러리는 get,path, status code이
 
 
 
-const home = (req, res)=>{//첫번째 오브젝트는req 두번째는 무조건res 오브젝트이다.
-    //next오브젝트는 다음 함수를 실행시켜준다.
+// const home = (req, res)=>{//첫번째 오브젝트는req 두번째는 무조건res 오브젝트이다.
+//     //next오브젝트는 다음 함수를 실행시켜준다.
     
-    return res.send("I love middlewares");//서버가 응답을 받으면 끝내준다. 서버가 request를 끝내버렸다.
-};
+//     return res.send("I love middlewares");//서버가 응답을 받으면 끝내준다. 서버가 request를 끝내버렸다.
+// };
 
-const login = (req, res) =>{
-    return res.send('login');
-}
+// const login = (req, res) =>{
+//     return res.send('login');
+// }
 
 //app.use(logger);//app.use는 글로벌 미들웨어를 만들수 있게 해준다. 모든 라우트(경로)마다 미들웨어를 호출해준다.
 //먼저 use가 나와야하고 그다음에 get 이 나와야한다.
 
-app.use(logger);
+
 //app.use(methodLogger, routeLogger);
-app.get("/", home);//get으로 누가 서버에 리퀘스트 하는지 가져온다. => 헤이 난 이 서버가 필요해 ("/"여기를 가려고하는구나, 이함수를 실행시켜 줄게)
-app.get("/login", login);
+// app.get("/", home);//get으로 누가 서버에 리퀘스트 하는지 가져온다. => 헤이 난 이 서버가 필요해 ("/"여기를 가려고하는구나, 이함수를 실행시켜 줄게)
+// app.get("/login", login);
 //모든 handler(controler)는 미들웨어다.
+
+
+
+
 
 const handleListening = () => console.log(`server listenting on port http://localhost:${PORT}`);
 
