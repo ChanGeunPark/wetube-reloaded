76e675e6a0a1ed10c34ddc716934433ca62d6eba
ChanGeunPark <design795@naver.com>
2022-01-14 23:03:21 +0900

    learn mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,33 @@
-export const trending = (req, res) => res.render("home",{pageTitle: "Home"});//view엔진이 pug로 작동한다는것을 알아서 렌더로  home을 불러올 수 잇다.
+
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title:"First Video",
+            rating:5,
+            comments:2,
+            createdAt: "2 minutes age",
+            views: 59,
+            id: 1,
+        },
+        {
+            title:"Second Video",
+            rating:5,
+            comments:2,
+            createdAt: "2 minutes age",
+            views: 59,
+            id: 1,
+        },
+        {
+            title:"Third Video",
+            rating:5,
+            comments:2,
+            createdAt: "2 minutes age",
+            views: 59,
+            id: 1,
+        }
+    ];
+    return res.render("home",{pageTitle: "Home", videos});//view엔진이 pug로 작동한다는것을 알아서 렌더로  home을 불러올 수 잇다.
+}
 //두번째 파라미터는 해당페이지에 보내줄 변수이다.
 
 export const see = (req,res) => res.render("watch");
--- src/server.js
+++ src/server.js
@@ -62,4 +62,4 @@ const handleListening = () => console.log(`server listenting on port http://loca
 
 app.listen(PORT, handleListening); //서버가 사람들이 뭔가를 요청할 때까지 기다리게 해준다. // 4000은 포트번호 handleListening은 콜백함수
 
-//5.7부터 다시보기
+//5.10부터 다시보기
